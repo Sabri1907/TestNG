b0bc9a59fe0a47e73c9339f1058b3349c607d18d
Sabri Guvendi <guvendisabri@gmail.com>
2022-10-05 20:49:46 +0300

    05.10.2022

--- src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
+++ src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
@@ -1,6 +1,9 @@
 package tests.day21;
 
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
@@ -17,8 +20,25 @@ public class C01_ScreenShotReusableMethod {
         ReusableMethods.getScreenshot("hepsiburada");
 
         // Sayfayi kapatin
-        Driver.getDriver().close();
+        //Driver.getDriver().close();
 
+    }
+
+    @Test
+    public void test02() throws IOException {
+        // Amazon sayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        // Nutella aratin
+        AmazonPage amazonPage=new AmazonPage();
+        amazonPage.aramaKutusu.sendKeys("Nutella", Keys.ENTER);
+
+        // Arama sonuc yazisi Webelementi'nin ekran goruntusunuz alin
+        ReusableMethods.getScreenshotWebElement("AramaSonucWE",amazonPage.aramaSonucuWE);
+        // => Bu method parametre olarak String isim ve webelemn'tin locate'ini istiyor.
+
+        // Sayfayi kapatin
+        Driver.getDriver().close();
 
 
     }
--- src/test/java/tests/day21/HomeWork.java
+++ src/test/java/tests/day21/HomeWork.java
@@ -1,11 +1,21 @@
 package tests.day21;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class HomeWork {
-    // Hepsiburada sayfasina gidiniz
+    @Test
+    public void test01() {
+        // Hepsiburada sayfasina gidiniz
+        Driver.getDriver().get(ConfigReader.getProperty("hepsiBuradaUrl"));
 
-    // ELektroni>Bilgisayar/Tablet altindaki tum linkleri tiklayin
+        // ELektroni>Bilgisayar/Tablet altindaki tum linkleri tiklayin
 
-    // Her link'ten sonra o sayfaya gittiginizi test edin ve o sayfanin ekran goruntusunu alin
+        // Her link'ten sonra o sayfaya gittiginizi test edin ve o sayfanin ekran goruntusunu aling
 
-    // Sayfayi kapatin
+        // Sayfayi kapatin
+
+    }
+
 }
